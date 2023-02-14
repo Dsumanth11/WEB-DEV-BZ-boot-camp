d2bc7387c6faae508877453503d6faacdd8a56ef
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-14 17:07:15 +0530

    abc

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app=express();
-// app.get('/',(request,response)=>{
-//     response.sendFile("./index.html",{root:__dirname});
-// });
+app.get('/',(request,response)=>{
+    response.sendFile("./index.html",{root:__dirname});
+});
 app.get('/',(request,response)=>{
     response.sendFile("PortFolio-main/index.html",{root:__dirname});
 })
