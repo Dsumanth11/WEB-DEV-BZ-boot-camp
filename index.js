156c9af0041a330c76b84e43251f1dfd650c3f95
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-14 21:15:15 +0530

    added resume 1.0

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express=require('express');
 const app=express();
-// app.get('/',(request,response)=>{
-//     response.sendFile("./index.html",{root:__dirname});
-// });
 app.get('/',(request,response)=>{
+    response.sendFile("index.html",{root:__dirname});
+});
+app.get('/resume',(request,response)=>{
     response.sendFile("PortFolio-main/index.html",{root:__dirname});
-})
+});
 app.listen(3000,()=>{
     console.log("Server started at port no 3000");
 });
