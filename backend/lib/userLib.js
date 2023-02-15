1b7a5ed673a40b3fda76df985fec0f6fe60bbe87
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-15 23:19:07 +0530

    added backend lib and userlib.js and wrote getall users create first users

---
+++ backend/lib/userLib.js
@@ -0,0 +1,29 @@
+const userModel=require("../models/userModel");
+
+module.exports.getAllUsers = async function(callback){
+    try{
+        var users=await userModel.find({});
+        callback(null,users);
+    }
+    catch(err)
+    {
+        callback(err,null);
+    }
+}
+
+module.exports.createFirstUser = async function(callback){
+    try{
+        var user={
+            username:"DSumanth",
+            yearOfGraduation:2024,
+        };
+        var newUser = await new userModel(user);
+        var result = await newUser.save();
+        var users = await userModel.find({});
+        callback(null,users);
+    }
+    catch(err)
+    {
+        callback(err,null);
+    }
+}
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
+const userlib=require("./backend/lib/userLib");
 const mongoose=require("mongoose");
 
 const express=require('express');
 const app=express();
+const port=process.env.PORT || 5010;
 app.get('/',(request,response)=>{
     response.sendFile("index.html",{root:__dirname});
 });
@@ -17,8 +19,21 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
     }
     else{
         console.log("DB connected");
+        userlib.getAllUsers(function(err,res){
+            if(res.length==0)
+            {
+                userlib.createFirstUser(function(err,res){
+                    if(err)
+                    {
+                        console.error(err);
+                    }
+                    else{
+                        console.log(res);
+                    }
+                });
+            }
+        });
         app.listen(3000,()=>{
-        
             console.log("Server started at port no 3000");
         });
     }
