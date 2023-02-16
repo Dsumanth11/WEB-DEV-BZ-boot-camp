048d4d1a4511be8a072fa1d205116ca78dfd3360
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-16 21:41:23 +0530

    abc

--- backend/lib/userLib.js
+++ backend/lib/userLib.js
@@ -2,7 +2,7 @@ const userModel=require("../models/userModel");
 
 module.exports.getAllUsers = async function(callback){
     try{
-        var users=await userModel.find({});
+        var users=await userModel.find({isDeleted:false});
         callback(null,users);
     }
     catch(err)
@@ -21,6 +21,17 @@ module.exports.createFirstUser = async function(callback){
         var result = await newUser.save();
         var users = await userModel.find({});
         callback(null,users);
+    }
+    catch(err)
+    {
+        callback(err,null);
+    }
+}
+
+module.exports.getuserByFilter=async function(filter,callback){
+    try{
+        var user=await userModel.find(filter);
+        callback(null,user);
     }
     catch(err)
     {
