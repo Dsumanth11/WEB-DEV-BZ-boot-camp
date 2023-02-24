645f0bc85798b6d666df0bae490fa2802c416463
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-24 20:51:55 +0530

    done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const userlib=require("./backend/lib/userLib");
+const todoLib=require("./backend/lib/todoLib");
 const mongoose=require("mongoose");
 const options={
     extensions:['htm','html','css','js','ico','jpg','jpeg','png','svg','pdf'],
@@ -7,6 +8,7 @@ const options={
 };
 const express=require('express');
 const app=express();
+app.use(express.json());
 app.use(express.static("public",options));
 const port=process.env.PORT || 3000;
 app.get('/',(request,response)=>{
@@ -19,19 +21,124 @@ app.get('/weather',(request,response)=>{
     response.sendFile("weather1.html",{root:__dirname});
 });
 
-app.get('/todolist',(request,response)=>{
+///---------------//
+app.get('/todo',(request,response)=>{
     response.sendFile("todolist.html",{root:__dirname});
 });
 
-// app.get("/api/todos",function(request,response){
-//     response.json({
-//         [
-//             {"name":"Dsumanth11","age":18},
-//             {"name":"Bittu","age":18},
-//             {"name":"Dontha","age":20},
-//         ]
-//     })
-// })
+app.get("/api/todos", function(req, res) {
+    todoLib.getAllTodos(function(err, todos) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+                data: null
+            });
+        } else {
+            res.json({
+                status: "success",
+                message: "Todos retrieved successfully",
+                data: todos
+            });
+        }
+    })
+});
+
+
+app.post("/api/todos", function(req, res) {
+    const todo = req.body;
+    todoLib.createTodo(todo, function(err, result) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+                data: null
+            });
+        } else {
+            res.json({
+                status: "success",
+                message: "Todo created successfully",
+                data: result
+            });
+        }
+    })
+});
+
+app.put("/api/todos/:id", function(req, res) {
+    const id = req.params.id;
+    const data = req.body;
+    todoLib.updateTodoById(id, data, function(err, result) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+                data: null
+            });
+        } else {
+            res.json({
+                status: "success",
+                message: "Todo updated successfully",
+                data: result
+            });
+        }
+    })
+});
+
+app.delete("/api/todos/:id", function(req, res) {
+    const id = req.params.id;
+    todoLib.deleteTodoById(id, function(err, result) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+                data: null
+            });
+        } else {
+            res.json({
+                status: "success",
+                message: "Todo deleted successfully",
+                data: result
+            });
+        }
+    })
+});
+
+app.get("/api/todos/completed", function(req, res) {
+    todoLib.getAllCompletedTodos(function(err, todos) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+                data: null
+            });
+        } else {
+            res.json({
+                status: "success",
+                message: "Deleted Todos retrieved successfully",
+                data: todos
+            });
+        }
+    })
+});
+
+app.get("/api/todos/deleted", function(req, res) {
+    todoLib.getAllDeletedTodos(function(err, todos) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+                data: null
+            });
+        } else {
+            res.json({
+                status: "success",
+                message: "Deleted Todos retrieved successfully",
+                data: todos
+            });
+        }
+    })
+});
+
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
     if(err)
@@ -99,9 +206,9 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
         //         console.log(result);
         //     }
         // });
-        app.listen(port,()=>{
-            console.log("Server running on http://localhost:"+port);
-			console.log(`Server running on http://localhost:${port}`);
+        app.listen((port+10),()=>{
+            console.log("Server running on http://localhost:"+(port+10));
+			console.log(`Server running on http://localhost:${port+10}`);
         });
     }
 });
