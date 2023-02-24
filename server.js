5d4bebbfa19c870f9fbfbacd54a580075d00a7a4
Dsumanth11 <sumanth.bittu100@gmail.com>
2023-02-24 20:56:57 +0530

    done

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const express=require('express');
 const app=express();
 app.use(express.json());
 app.use(express.static("public",options));
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3010;
 app.get('/',(request,response)=>{
     response.sendFile("index.html",{root:__dirname});
 });
@@ -206,9 +206,9 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
         //         console.log(result);
         //     }
         // });
-        app.listen((port+10),()=>{
-            console.log("Server running on http://localhost:"+(port+10));
-			console.log(`Server running on http://localhost:${port+10}`);
+        app.listen((port),()=>{
+            console.log("Server running on http://localhost:"+(port));
+			console.log(`Server running on http://localhost:${port}`);
         });
     }
 });
